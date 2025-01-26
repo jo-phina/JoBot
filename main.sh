247762764ab3144cdf2a00dd2c866c1837570ced
Josephina Imhoff <josephina.imhoff@posteo.de>
2025-01-26 17:21:14 +0100

    added main.sh to run

---
+++ main.sh
@@ -0,0 +1,6 @@
+# make sure, you have an python file config.py where you enter the necessary APIs:
+# TELEGRAM_API = "" and LOCAL_SERVER_API = ""
+
+#!/bin/bash
+pip install -r requirements.txt
+python ./main.py
